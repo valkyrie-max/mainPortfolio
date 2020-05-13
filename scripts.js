b1c40f6e70b11e135b835ca5ede8f8805d07266e
valkyrie-max <alisakitkina2016@gmail.com>
2020-05-13 14:20:09 -0500

    added carousel; working on adding content

---
+++ scripts.js
@@ -0,0 +1,10 @@
+$(function() {
+    console.log('ready');
+    $('.gallery').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: true,
+        dots: false,
+        speed: 1000,
+    });
+});

e850e967a259693fbe108adbc9c03fee197c6f3d
valkyrie-max <alisakitkina2016@gmail.com>
2020-05-26 17:13:41 -0500

    finished restyling the landing page

--- scripts.js
+++ scripts.js
@@ -1,19 +1,8 @@
 $(function() {
-    console.log('ready');
-    $('.gallery').slick({
-        autoplay: true,
-        autoplaySpeed: 5000,
-        arrows: false,
-        dots: false,
-        speed: 1000,
-    });
-
-
     $('a[href^="#"]').click(function () {
         $('html, body').animate({
             scrollTop: $( $.attr(this, 'href') ).offset().top
-        }, 1200);
+        }, 1500);
         return false;
     });
-
 });
